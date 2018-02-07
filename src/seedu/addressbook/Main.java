f23927a18bb62fb1d3d32c39555132ea87c62222
Charmaine Lee <e0052642@u.nus.edu>
2018-02-07 10:14:47 +0800

    handle read only storage file exception

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -106,10 +106,14 @@ public class Main {
      * @return result of the command
      */
     private CommandResult executeCommand(Command command)  {
+        command.setData(addressBook, lastShownList);
+        CommandResult result = command.execute();
+
         try {
-            command.setData(addressBook, lastShownList);
-            CommandResult result = command.execute();
             storage.save(addressBook);
+            return result;
+        } catch (StorageOperationException e) {
+            ui.showToUser(e.getMessage());
             return result;
         } catch (Exception e) {
             ui.showToUser(e.getMessage());
